273dd1fbb562b1c04f52f0aa901ddb65893da230
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2019-04-15 20:49:09 +0200

    add assets

---
+++ Assets/EyeTracking.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.XR.MagicLeap;
+
+public class EyeTracking : MonoBehaviour
+{
+    public GameObject Camera;
+    public Material FocusedMaterial, NonFocusedMaterial;
+
+    private Vector3 heading;
+    private MeshRenderer meshRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MLEyes.Start();
+        transform.position = Camera.transform.position + Camera.transform.forward * 2.0f;
+
+        meshRenderer = gameObject.GetComponent<MeshRenderer>();
+    }
+
+    private void OnDisable()
+    {
+        MLEyes.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (MLEyes.IsStarted)
+        {
+            RaycastHit rayHit;
+            heading = MLEyes.FixationPoint - Camera.transform.position;
+
+            if (Physics.Raycast(Camera.transform.position, heading, out rayHit, 10.0f))
+            {
+                meshRenderer.material = FocusedMaterial;
+            }
+            else
+            {
+                meshRenderer.material = NonFocusedMaterial;
+            }
+        }
+    }
+}
