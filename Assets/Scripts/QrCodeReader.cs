264efb5d488ff17fbcacaf2db183c21050403149
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2019-04-16 15:56:47 +0200

    further tries with decoding of qr code.

--- Assets/Scripts/QrCodeReader.cs
+++ Assets/Scripts/QrCodeReader.cs
@@ -1,18 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
-using TMPro;
-using ZXing;
-using ZXing.QrCode;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.XR.MagicLeap;
+using ZXing;
 
 public class QrCodeReader : MonoBehaviour
 {
-    public TextMeshPro title;
+    [Serializable]
+    private class ImageCaptureEvent : UnityEvent<Texture2D>
+    {
+    }
 
     #region Private Variables
-  private bool _isCameraConnected = false;
-    private bool _isCapturing = false;
+
+    [SerializeField] private ImageCaptureEvent OnImageReceivedEvent;
+
+    private bool _isCameraConnected;
+    private bool _isCapturing;
 
     private enum PrivilegeState
     {
@@ -25,40 +30,41 @@ public class QrCodeReader : MonoBehaviour
 
     private PrivilegeState _currentPrivilegeState = PrivilegeState.Off;
 
-    private MLPrivilegeId[] _privilegesNeeded = { MLPrivilegeId.CameraCapture };
+    private readonly MLPrivilegeId[] _privilegesNeeded = {MLPrivilegeId.CameraCapture};
 
-    private List<MLPrivilegeId> _privilegesGranted = new List<MLPrivilegeId>();
+    private readonly List<MLPrivilegeId> _privilegesGranted = new List<MLPrivilegeId>();
 
-    private bool _hasStarted = false;
+    private bool _hasStarted;
+
     #endregion
 
     #region Unity Methods
-    /// <summary>
-    /// Start Privilege API.
-    /// </summary>
-    void OnEnable()
-    {
-        Debug.LogError("startup.");
 
-        MLResult result = MLPrivileges.Start();
+    /// <summary>
+    ///     Start Privilege API.
+    /// </summary>
+    private void OnEnable()
+    {
+        var result = MLPrivileges.Start();
         if (result.IsOk)
         {
             _currentPrivilegeState = PrivilegeState.Started;
-        } else
+        }
+        else
         {
             Debug.LogError("Privilege Error: failed to startup");
             enabled = false;
-            return;
         }
     }
 
     /// <summary>
-    /// Stop the camera, unregister callbacks, and stop input and privileges APIs.
+    ///     Stop the camera, unregister callbacks, and stop input and privileges APIs.
     /// </summary>
-    void OnDisable()
+    private void OnDisable()
     {
         if (MLInput.IsStarted)
         {
+            MLInput.OnControllerButtonDown -= OnButtonDown;
             MLInput.Stop();
         }
 
@@ -75,15 +81,14 @@ public class QrCodeReader : MonoBehaviour
 
             _currentPrivilegeState = PrivilegeState.Off;
             _privilegesGranted.Clear();
-
         }
     }
 
     /// <summary>
-    /// Cannot make the assumption that a reality privilege is still granted after
-    /// returning from pause. Return the application to the state where it
-    /// requests privileges needed and clear out the list of already granted
-    /// privileges. Also, disable the camera and unregister callbacks.
+    ///     Cannot make the assumption that a reality privilege is still granted after
+    ///     returning from pause. Return the application to the state where it
+    ///     requests privileges needed and clear out the list of already granted
+    ///     privileges. Also, disable the camera and unregister callbacks.
     /// </summary>
     private void OnApplicationPause(bool pause)
     {
@@ -102,48 +107,47 @@ public class QrCodeReader : MonoBehaviour
                 DisableMLCamera();
             }
 
+            MLInput.OnControllerButtonDown -= OnButtonDown;
+
             _hasStarted = false;
         }
     }
 
     /// <summary>
-    /// Move through the privilege stages before enabling the feature that requires privileges.
+    ///     Move through the privilege stages before enabling the feature that requires privileges.
     /// </summary>
-    void Update()
+    private void Update()
     {
         /// Privileges have not yet been granted, go through the privilege states.
-        if (_currentPrivilegeState != PrivilegeState.Granted)
-        {
-            UpdatePrivilege();
-        }
+        if (_currentPrivilegeState != PrivilegeState.Granted) UpdatePrivilege();
         /// Privileges have been granted, enable the feature and run any normal updates items.
         /// Done in a seperate if statement so enable can be done in the same frame as the
         /// privilege is granted.
-        if (_currentPrivilegeState == PrivilegeState.Granted)
-        {
-            StartCapture();
-        }
+        if (_currentPrivilegeState == PrivilegeState.Granted) StartCapture();
     }
+
     #endregion
 
     #region Public Methods
+
     /// <summary>
-    /// Connects the MLCamera component and instantiates a new instance
-    /// if it was never created.
+    ///     Connects the MLCamera component and instantiates a new instance
+    ///     if it was never created.
     /// </summary>
     public bool EnableMLCamera()
     {
-        MLResult result = MLCamera.Start();
+        var result = MLCamera.Start();
         if (result.IsOk)
         {
             result = MLCamera.Connect();
             _isCameraConnected = result.IsOk;
         }
+
         return _isCameraConnected;
     }
 
     /// <summary>
-    /// Disconnects the MLCamera if it was ever created or connected.
+    ///     Disconnects the MLCamera if it was ever created or connected.
     /// </summary>
     public void DisableMLCamera()
     {
@@ -154,59 +158,79 @@ public class QrCodeReader : MonoBehaviour
     }
 
     /// <summary>
-    /// Captures a still image using the device's camera and returns
-    /// the data path where it is saved.
+    ///     Captures a still image using the device's camera and returns
+    ///     the data path where it is saved.
     /// </summary>
     /// <param name="fileName">The name of the file to be saved to.</param>
     public void TriggerAsyncCapture()
     {
         if (MLCamera.IsStarted && _isCameraConnected)
         {
-            MLResult result = MLCamera.CaptureRawImageAsync();
-            if (result.IsOk)
-            {
-                _isCapturing = true;
-            }
+            var result = MLCamera.CaptureRawImageAsync();
+            if (result.IsOk) _isCapturing = true;
         }
     }
+
     #endregion
 
     #region Event Handlers
 
     /// <summary>
-    /// Handles the event of a new image getting captured.
+    ///     Handles the event for button down.
+    /// </summary>
+    /// <param name="controllerId">The id of the controller.</param>
+    /// <param name="button">The button that is being pressed.</param>
+    private void OnButtonDown(byte controllerId, MLInputControllerButton button)
+    {
+        if (MLInputControllerButton.Bumper == button && !_isCapturing) TriggerAsyncCapture();
+    }
+
+    /// <summary>
+    ///     Handles the event of a new image getting captured.
     /// </summary>
     /// <param name="imageData">The raw data of the image.</param>
     private void OnCaptureRawImageComplete(byte[] imageData)
     {
+        Debug.Log("Image captured.");
+
         _isCapturing = false;
-
-        // Initialize to 8x8 texture so there is no discrepency
-        // between uninitalized captures and error texture
-        Texture2D texture = new Texture2D(8, 8);
-        bool status = texture.LoadImage(imageData);
-
         try
         {
+            // Initialize to 8x8 texture so there is no discrepency
+            // between uninitalized captures and error texture
+            var texture = new Texture2D(8, 8);
+            var status = texture.LoadImage(imageData);
+
             IBarcodeReader barcodeReader = new BarcodeReader();
-            var result = barcodeReader.Decode(texture.GetPixels32(),
-                texture.width, texture.height);
+            var result = barcodeReader.Decode(imageData,
+                texture.width, texture.height, 
+                RGBLuminanceSource.BitmapFormat.RGB565);
+
             if (result != null)
             {
-                title.text = result.Text;
                 Debug.Log("DECODED TEXT FROM QR: " + result.Text);
-                MLCamera.StopPreview();
             }
+            else
+            {
+                Debug.Log("Decoding Failed.");
+            }
+
+            if (status && texture.width != 8 && texture.height != 8) OnImageReceivedEvent.Invoke(texture);
+
         } catch (Exception ex)
         {
+            Debug.LogWarning("Something went wrong with decoding.");
             Debug.LogWarning(ex.Message);
         }
+
     }
+
     #endregion
 
     #region Private Functions
+
     /// <summary>
-    /// Handle the privilege states.
+    ///     Handle the privilege states.
     /// </summary>
     private void UpdatePrivilege()
     {
@@ -214,40 +238,36 @@ public class QrCodeReader : MonoBehaviour
         {
             /// Privilege API has been started successfully, ready to make requests.
             case PrivilegeState.Started:
-                {
-                    RequestPrivileges();
-                    break;
-                }
+            {
+                RequestPrivileges();
+                break;
+            }
             /// Privilege requests have been made, wait until all privileges are granted before enabling the feature that requires privileges.
             case PrivilegeState.Requested:
-                {
-                    foreach (MLPrivilegeId priv in _privilegesNeeded)
-                    {
-                        if (!_privilegesGranted.Contains(priv))
-                        {
-                            return;
-                        }
-                    }
-                    _currentPrivilegeState = PrivilegeState.Granted;
-                    break;
-                }
+            {
+                foreach (var priv in _privilegesNeeded)
+                    if (!_privilegesGranted.Contains(priv))
+                        return;
+                _currentPrivilegeState = PrivilegeState.Granted;
+                break;
+            }
             /// Privileges have been denied, respond appropriately.
             case PrivilegeState.Denied:
-                {
-                    enabled = false;
-                    break;
-                }
+            {
+                enabled = false;
+                break;
+            }
         }
     }
 
     /// <summary>
-    /// Once privileges have been granted, enable the camera and callbacks.
+    ///     Once privileges have been granted, enable the camera and callbacks.
     /// </summary>
     private void StartCapture()
     {
         if (!_hasStarted)
         {
-            MLResult result = MLInput.Start();
+            var result = MLInput.Start();
             if (!result.IsOk)
             {
                 Debug.LogError("Failed to start MLInput on ImageCapture component. Disabling the script.");
@@ -262,20 +282,21 @@ public class QrCodeReader : MonoBehaviour
                 return;
             }
 
+            MLInput.OnControllerButtonDown += OnButtonDown;
             MLCamera.OnRawImageAvailable += OnCaptureRawImageComplete;
+
             _hasStarted = true;
-            TriggerAsyncCapture();
         }
     }
 
     /// <summary>
-    /// Request each needed privilege.
+    ///     Request each needed privilege.
     /// </summary>
     private void RequestPrivileges()
     {
-        foreach (MLPrivilegeId priv in _privilegesNeeded)
+        foreach (var priv in _privilegesNeeded)
         {
-            MLResult result = MLPrivileges.RequestPrivilegeAsync(priv, HandlePrivilegeAsyncRequest);
+            var result = MLPrivileges.RequestPrivilegeAsync(priv, HandlePrivilegeAsyncRequest);
             if (!result.IsOk)
             {
                 Debug.LogErrorFormat("{0} Privilege Request Error: {1}", priv, result);
@@ -288,10 +309,10 @@ public class QrCodeReader : MonoBehaviour
     }
 
     /// <summary>
-    /// Handles the result that is received from the query to the Privilege API.
-    /// If one of the required privileges are denied, set the Privilege state to Denied.
-    /// <param name="result">The resulting status of the query</param>
-    /// <param name="privilegeId">The privilege being queried</param>
+    ///     Handles the result that is received from the query to the Privilege API.
+    ///     If one of the required privileges are denied, set the Privilege state to Denied.
+    ///     <param name="result">The resulting status of the query</param>
+    ///     <param name="privilegeId">The privilege being queried</param>
     /// </summary>
     private void HandlePrivilegeAsyncRequest(MLResult result, MLPrivilegeId privilegeId)
     {
@@ -299,11 +320,13 @@ public class QrCodeReader : MonoBehaviour
         {
             _privilegesGranted.Add(privilegeId);
             Debug.LogFormat("{0} Privilege Granted", privilegeId);
-        } else
+        }
+        else
         {
             Debug.LogErrorFormat("{0} Privilege Error: {1}, disabling example.", privilegeId, result);
             _currentPrivilegeState = PrivilegeState.Denied;
         }
     }
+
     #endregion
 }
