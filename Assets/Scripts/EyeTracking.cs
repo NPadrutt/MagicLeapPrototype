8234d2b396d16192b2243469ee28165ecfb76327
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2019-04-29 18:44:36 +0200

    add enum for sides.

--- Assets/Scripts/EyeTracking.cs
+++ Assets/Scripts/EyeTracking.cs
@@ -4,14 +4,24 @@ using UnityEngine.XR.MagicLeap;
 
 namespace Assets.Scripts
 {
+    public enum DetailInfoSide
+    {
+        Left,
+        Right,
+    }
+
     public class EyeTracking : MonoBehaviour
     {
         public Material FocusedMaterial, NonFocusedMaterial;
         public GameObject DetailObjectToOpen;
 
-        private Vector3 heading;
-        private MeshRenderer meshRenderer;
-        private bool isDetailOpen;
+        [Tooltip("Specifies the axis about which the object will rotate.")]
+        [SerializeField]
+        private DetailInfoSide detailSide = DetailInfoSide.Left;
+        public DetailInfoSide DetailSide {
+            get { return detailSide; }
+            set { detailSide = value; }
+        }
 
         [Tooltip("Specifies the target we will orient to. If no target is specified, the main camera will be used.")]
         private Transform targetTransform;
@@ -19,6 +29,12 @@ namespace Assets.Scripts
             get { return targetTransform; }
             set { targetTransform = value; }
         }
+
+        private Vector3 heading;
+        private MeshRenderer meshRenderer;
+        private bool isDetailOpen;
+
+
 
         // Start is called before the first frame update
         void Start() 
@@ -65,7 +81,7 @@ namespace Assets.Scripts
                         if (DetailObjectToOpen != null && !isDetailOpen)
                         {
                             var heading = TargetTransform.position + TargetTransform.forward * 2;
-                            heading.x = -1.1f;
+                            heading.x = DetailSide == DetailInfoSide.Left ? -1.1f : 0.7f;
                             heading.y = 0;
 
                             DetailObjectToOpen.transform.position = heading;
