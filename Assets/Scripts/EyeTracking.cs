707f42e106e4051363e7e39adae76f9abc3569dd
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2019-04-30 16:50:28 +0200

    improve opening of detail window

--- Assets/Scripts/EyeTracking.cs
+++ Assets/Scripts/EyeTracking.cs
@@ -14,17 +14,20 @@ namespace Assets.Scripts
         public Material FocusedMaterial, NonFocusedMaterial;
         public GameObject DetailObjectToOpen;
 
-        [Tooltip("Specifies the axis about which the object will rotate.")]
-        [SerializeField]
+        [Tooltip("Specifies the axis about which the object will rotate.")] [SerializeField]
         private DetailInfoSide detailSide = DetailInfoSide.Left;
-        public DetailInfoSide DetailSide {
+
+        public DetailInfoSide DetailSide
+        {
             get { return detailSide; }
             set { detailSide = value; }
         }
 
         [Tooltip("Specifies the target we will orient to. If no target is specified, the main camera will be used.")]
         private Transform targetTransform;
-        public Transform TargetTransform {
+
+        public Transform TargetTransform
+        {
             get { return targetTransform; }
             set { targetTransform = value; }
         }
@@ -34,21 +37,23 @@ namespace Assets.Scripts
         private bool isDetailOpen;
 
 
-
         // Start is called before the first frame update
-        void Start() 
+        void Start()
         {
-            if (DetailObjectToOpen != null)
+            if (TargetTransform == null)
             {
-                DetailObjectToOpen.SetActive(false);
-            }
-
-            if (TargetTransform == null) {
                 TargetTransform = CameraCache.Main.transform;
             }
 
             MLEyes.Start();
             meshRenderer = gameObject.GetComponent<MeshRenderer>();
+        }
+
+        void Awake()
+        {
+            if (DetailObjectToOpen != null) {
+                DetailObjectToOpen.SetActive(false);
+            }
         }
 
         private void OnDisable()
@@ -71,35 +76,37 @@ namespace Assets.Scripts
                 RaycastHit rayHit;
                 heading = MLEyes.FixationPoint - TargetTransform.position;
 
-                if (Physics.Raycast(TargetTransform.position, heading, out rayHit, 10.0f)) {
-
+                if (Physics.Raycast(TargetTransform.position, heading, out rayHit, 10.0f))
+                {
                     if (rayHit.transform.position == gameObject.transform.position)
                     {
                         meshRenderer.material = FocusedMaterial;
 
                         if (DetailObjectToOpen != null && !isDetailOpen)
                         {
-                            var heading = TargetTransform.position + TargetTransform.forward * 2;
-                            heading.x = DetailSide == DetailInfoSide.Left ? -1.1f : 0.7f;
-                            heading.y = 0;
+                            var heading = transform.position;
+                            heading.x += DetailSide == DetailInfoSide.Left ? -0.6f : 0.6f;
 
                             DetailObjectToOpen.transform.position = heading;
                             DetailObjectToOpen.SetActive(true);
                             isDetailOpen = true;
                         }
                     }
-                    else if (isDetailOpen && rayHit.transform.position != DetailObjectToOpen.transform.position)
+                    else if (isDetailOpen && rayHit.transform.position == DetailObjectToOpen.transform.position)
+                    {
+                        meshRenderer.material = NonFocusedMaterial;
+                    }
+                    else
                     {
                         DetailObjectToOpen.SetActive(false);
                         isDetailOpen = false;
                         meshRenderer.material = NonFocusedMaterial;
                     }
-                    else {
-                        meshRenderer.material = NonFocusedMaterial;
-                    }
-                } 
-                else 
+                }
+                else
                 {
+                    DetailObjectToOpen.SetActive(false);
+                    isDetailOpen = false;
                     meshRenderer.material = NonFocusedMaterial;
                 }
             }
